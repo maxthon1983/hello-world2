9e61250b70b313edf10d3a2976b9616c2363cdfa
zhouhongfu <zhouhongfu@zhouhongfudeMacBook-Pro.local>
2017-03-05 20:36:08 +0800

    test

---
+++ src/com/bj58/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.bj58.demo;
+
+/**
+ * Created by zhouhongfu on 17/3/5.
+ */
+public class HelloWorld {
+    public  static  void  main(String[] args){
+        System.out.println("hello world");
+
+    }
+}
